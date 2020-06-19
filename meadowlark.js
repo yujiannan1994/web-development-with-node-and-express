e166cabd53aff1ee428b41d61871d53d9c278bc3
yujiannan1994 <yujiannan1994@gmail.com>
2020-06-19 09:43:13 +0800

    ch08

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,9 @@
-var express = require('express');
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	formidable = require('formidable'),
+	jqupload = require('jquery-file-upload-middleware');
 
 var app = express();
-
-var fortune = require('./lib/fortune.js');
 
 // 设置 handlebars 视图引擎
 var handlebars = require('express3-handlebars')
@@ -22,6 +23,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
@@ -62,6 +64,20 @@ app.use(function(req, res, next){
 	if(!res.locals.partials) res.locals.partials = {};
  	res.locals.partials.weatherContext = getWeatherData();
  	next();
+});
+
+// jQuery File Upload endpoint middleware
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function(){
+            console.log("__dirname: " + __dirname);
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function(){
+            return '/uploads/' + now;
+        },
+    })(req, res, next);
 });
 
 app.get('/', function(req, res) {
@@ -106,6 +122,45 @@ app.get('/data/nursery-rhyme', function(req, res){
 		adjective: 'bushy',
 		noun: 'heck',
 	});
+});
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+// app.post('/process', function(req, res){
+//     console.log('Form (from querystring): ' + req.query.form);
+//     console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+//     console.log('Name (from visible form field): ' + req.body.name);
+//     console.log('Email (from visible form field): ' + req.body.email);
+//     res.redirect(303, '/thank-you');
+// });
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 // 定制 404 页面
